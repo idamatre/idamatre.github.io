a23687542b1f394c8255f6ceb329ede665cac0b2
idamatre <ida.matre@gmail.com>
2025-09-21 21:44:17 +0200

    update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // enables `next export` (static site)
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
